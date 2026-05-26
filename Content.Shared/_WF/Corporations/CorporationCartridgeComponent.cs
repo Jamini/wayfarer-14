4f1e1a0421945ae322e794c5553ef0c30dbb2ad8
Shawn McHenry <drraccoony@gmail.com>
2026-05-25 21:05:06 -0500

    Revert "Corporate Drift Update (#743)"
    This reverts commit 94558e73d8382fecc7e7b5a3c2439c224feec56f.

--- Content.Shared/_WF/Corporations/CorporationCartridgeComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Shared._WF.Corporations;
-
-/// <summary>
-/// Marker component placed on the corporation cartridge entity.
-/// </summary>
-[RegisterComponent]
-public sealed partial class CorporationCartridgeComponent : Component
-{
-}
